f771be98c8e90487af563a8349aac1753c20ec9f
yashuwantjohn19 <yashuwant.john.s69@kalvium.community>
2026-06-12 14:00:25 +0530

    Ignore ESLint and TS errors during builds on Vercel

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
   turbopack: {
     // set root to this project to avoid workspace root inference warnings
     root: './'
